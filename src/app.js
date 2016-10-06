a9363f0685f3f984b77d287ae4a9be05d9738278
nbkhope <ryumemaru@yahoo.com>
2016-10-06 06:45:32 -0700

    Add firebase initialization in componentWillMount

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
 import firebase from 'firebase';
 
+import { Header } from './components/common';
+
 class App extends Component {
+  componentWillMount() {
+    // set up firebase
+    firebase.initializeApp({
+    apiKey: "AIzaSyCVfI9F3y5U7AWoDVLZMEKurkzZ-vDDnAM",
+    authDomain: "mobile-auth-d87b3.firebaseapp.com",
+    databaseURL: "https://mobile-auth-d87b3.firebaseio.com",
+    storageBucket: "mobile-auth-d87b3.appspot.com",
+    messagingSenderId: "737921963466"
+  });
+  }
+
   render() {
     return (
       <View>
