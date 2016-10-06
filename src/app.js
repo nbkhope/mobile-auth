18c8e5a0b97cb04d12c85f7d375bb40d455b6649
nbkhope <ryumemaru@yahoo.com>
2016-10-06 06:43:22 -0700

    Use Header component in App component; import firebase module

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
   render() {
     return (
       <View>
-        <Text>Mobile Auth</Text>
+        <Header title="Mobile Auth" />
+        <Text>Welcome!</Text>
       </View>
     );
   }
