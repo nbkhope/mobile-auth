acfa459fc0227e2aead868fc7ece25481f422919
nbkhope <ryumemaru@yahoo.com>
2016-10-09 12:41:55 -0700

    Apply style to View inside Spinner

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -3,7 +3,7 @@ import { View, ActivityIndicator } from 'react-native';
 
 const Spinner = ({ size }) => {
   return (
-    <View>
+    <View style={styles.spinnerStyle}>
       <ActivityIndicator size={size || 'large'} />
     </View>
   );
