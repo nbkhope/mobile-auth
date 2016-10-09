9807c479369795d23cc9e67bbd1698b601f8d3c5
nbkhope <ryumemaru@yahoo.com>
2016-10-09 12:46:24 -0700

    Add event handler to set state variable loggedIn to true/false depending on wheather user is logged in/out

--- src/app.js
+++ src/app.js
@@ -6,15 +6,29 @@ import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {
+    loggedIn: false,
+  };
+
   componentWillMount() {
     // set up firebase
     firebase.initializeApp({
-    apiKey: "AIzaSyCVfI9F3y5U7AWoDVLZMEKurkzZ-vDDnAM",
-    authDomain: "mobile-auth-d87b3.firebaseapp.com",
-    databaseURL: "https://mobile-auth-d87b3.firebaseio.com",
-    storageBucket: "mobile-auth-d87b3.appspot.com",
-    messagingSenderId: "737921963466"
-  });
+      apiKey: "AIzaSyCVfI9F3y5U7AWoDVLZMEKurkzZ-vDDnAM",
+      authDomain: "mobile-auth-d87b3.firebaseapp.com",
+      databaseURL: "https://mobile-auth-d87b3.firebaseio.com",
+      storageBucket: "mobile-auth-d87b3.appspot.com",
+      messagingSenderId: "737921963466"
+    });
+
+    // event handler to know when user has signed in/out
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      }
+      else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
   render() {
