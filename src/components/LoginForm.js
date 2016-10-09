9560fd85895c38d735796ad0138b6c9ace35007e
nbkhope <ryumemaru@yahoo.com>
2016-10-09 12:25:59 -0700

    Add error message and initial code to authenticate with firebase

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
-// import { View } from 'react-native';
+import { Text } from 'react-native';
 import { Card, CardSection, Button, Input } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: '',
+    error: '',
   };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    // clear up error message
+    this.setState({ error: '' });
+
+    // Authenticate with Firebase
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        // Attempt to create an account if user does not exist
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: "Authentication Failed." });
+          })
+          ;
+      })
+      ;
+  }
+
+  renderErrorMessage() {
+    if (this.state.error !== '') {
+      return (
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+      );
+    }
+  }
 
   render() {
     return (
@@ -30,8 +61,10 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        {this.renderErrorMessage()}
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
@@ -39,5 +72,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
+};
 
 export default LoginForm;
